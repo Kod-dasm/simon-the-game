14cc105837cd0736057f5f413f54ee083f96574e
Kod-dasm <54396012+Kod-dasm@users.noreply.github.com>
2022-01-26 20:01:39 +0500

    added game mechanics

--- src/App/App.js
+++ src/App/App.js
@@ -1,48 +1,111 @@
 export default {
   data() {
     return {
-      lists: [],
+      setTones: [],
+      userSetTones: [],
+      speed: 500,
+      intervalId: null,
+
+      level: [ 
+        { nameLevel: 'easy', speed: 1500 },
+        { nameLevel: 'normal', speed: 1000 },
+        { nameLevel: 'hard', speed: 400 }
+      ],
+
+      showLoss: false,
+      aGreen: false,
+      aRed: false,
+      aBlue: false,
+      aYellow: false,
+      playingSetTones: false,
     };
   },
   methods: {
-    removeRectangle(item) {
-      item.count >= 0 ? item.count-- : 0;
+    starGame() {
+      this.newGame()
+      this.newRound()
+      console.log("Array Color", this.setTones)
     },
-    addList() {
-      this.lists.push({
-        name: "List " + (this.lists.length + 1),
-        isOpen: false,
-        isVisible: false,
-        items: this.addItems(),
-      });
+    addTone() {
+      this.setTones.push(this.randomTone())
     },
-    addItems() {
-      let items = [];
-      const count = this.calculateRandomNumber();
-      for (let i = 0; i <= count; i++) {
-        items.push(this.addItem(i));
+    randomTone() {
+      return Math.floor(Math.random() * 4) + 1
+    },
+    showSetTones() {
+      let numberTone = 0
+      this.intervalId = setInterval(() => {
+        this.showTone(this.setTones[numberTone])
+        setTimeout(this.clearActive, this.speed/2)
+        if(numberTone >= this.setTones.length) {
+          this.playingSetTones = false
+          clearInterval(this.intervalId)
+        }
+        numberTone++
+      }, this.speed)
+    },
+    showTone(tone) {
+      switch (tone) {
+        case 1:
+          this.$refs.sound1.pause()
+          this.$refs.sound1.currentTime = 0
+          this.$refs.sound1.play()
+          this.aGreen = true
+          break;
+        case 2:
+          this.$refs.sound2.pause()
+          this.$refs.sound2.currentTime = 0
+          this.$refs.sound2.play()
+          this.aRed = true
+          break;
+        case 3:
+          this.$refs.sound4.pause()
+          this.$refs.sound4.currentTime = 0
+          this.$refs.sound4.play()
+          this.aBlue = true
+          break;
+        case 4:
+          this.$refs.sound3.pause()
+          this.$refs.sound3.currentTime = 0
+          this.$refs.sound3.play()
+          this.aYellow = true
+          break;
       }
-      return items;
     },
-    addItem(itemCount = 0) {
-      let _color = ((Math.random() * 0xffffff) << 0).toString(16);
-      _color.length < 6
-        ? (_color += ((Math.random() * 0xf) << 0).toString(16))
-        : _color;
-      return {
-        name: "item " + (itemCount + 1),
-        count: this.calculateRandomNumber(),
-        color: "#" + _color,
-        isVisible: false,
-      };
+    clearActive() {
+      this.aGreen = false
+      this.aRed = false
+      this.aYellow = false
+      this.aBlue = false
     },
-    calculateRandomNumber() {
-      return Math.floor(Math.random() * (10 - 3)) + 3;
+    intutUserSetTones(tone) {
+      if(!this.playingSetTones && !this.showLoss) {
+        this.showTone(tone)
+        setTimeout(this.clearActive, 100)
+        this.userSetTones.push(tone)
+        let countUserTones = this.userSetTones.length - 1
+        if(this.userSetTones[countUserTones] != this.setTones[countUserTones]) {
+          this.showLoss = true
+        }
+        else if(countUserTones + 1 == this.setTones.length) {
+          this.newRound()
+        }
+        console.log('My tones', this.userSetTones)
+      }
     },
-  },
-  created: function () {
-    for (let listCount = 0; listCount < 3; listCount++) {
-      this.addList();
+    newGame() {
+      clearInterval(this.intervalId)
+      this.clearActive()
+      this.setTones = []
+      this.userSetTones = []
+      this.showLoss = false
+      this.playingSetTones = false
+    },
+    newRound() {
+      this.userSetTones = []
+      this.playingSetTones = true
+      this.addTone()
+      this.showSetTones()
     }
   },
 };
